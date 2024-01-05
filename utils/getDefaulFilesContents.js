85bdbafd356a356d602ff058fb7efe9a72109527
unknown <dimaslubitsk@gmail.com>
2024-01-06 01:44:01 +0200

    master: created query file

--- utils/getDefaulFilesContents.js
+++ utils/getDefaulFilesContents.js
@@ -2,7 +2,7 @@ import { readFileContent } from "./readFileContent.js";
 import { getFilesContents } from "./getFilesContents.js";
 import { PATH_TO_FILENAMES_FILE } from "../const/const.js";
 
-export const getDefaulFileNames = () => {
-    const fileNamesString = readFileContent(PATH_TO_FILENAMES_FILE)
+export const getDefaulFilesContents = (filePath = PATH_TO_FILENAMES_FILE) => {
+    const fileNamesString = readFileContent(filePath)
     return getFilesContents(fileNamesString.split(' '))
 }
--- utils/readFiles.js
+++ utils/readFiles.js
@@ -1,10 +1,12 @@
 import { getFileNamesInFolder } from "./getFileNamesInFolder.js";
 import { argsList } from "./readComandLine.js";
 import { getFilesContents } from "./getFilesContents.js";
-import { getDefaulFileNames } from "./getDefaultFileNames.js";
+import { getDefaulFilesContents } from "./getDefaulFilesContents.js";
+import { PATH_TO_FILENAMES_FILE } from "../const/const.js";
 
 const isComandLineWithFolderName = argsList.length === 1 && argsList[0].includes('./')
-const isCommandLineWithFileNames = argsList.length > 0 && !argsList[0].includes('./') && argsList.every(arg => arg.includes('.'))
+const isCommandLineWithFileNames = argsList.length && !argsList[0].includes('./') && argsList.every(arg => arg.includes('.'))
+const isCommandLineWithOptions = argsList.length > 1 && argsList.some(arg => arg.includes('./')) && argsList.includes('-n')
 
 export const readFiles = () => {
     if (isComandLineWithFolderName) {
@@ -13,6 +15,16 @@ export const readFiles = () => {
         return getFilesContents(fileNamesToRead, folderToRead)
     } else if (isCommandLineWithFileNames)
         return getFilesContents(argsList)
-    else
-        return getDefaulFileNames()
+    else if (isCommandLineWithOptions) {
+        const definedFileNamesOptionIndex = argsList.findIndex(arg => arg === '-n')
+        const definedFileNamesOption = (
+            definedFileNamesOptionIndex !== argsList.length - 1 
+            && definedFileNamesOptionIndex !== -1
+            && argsList[definedFileNamesOptionIndex + 1]
+        ) ? argsList[definedFileNamesOptionIndex + 1] : PATH_TO_FILENAMES_FILE
+        console.log(definedFileNamesOption)
+        return getDefaulFilesContents(definedFileNamesOption) 
+
+    } else
+        return getDefaulFilesContents()
 }
